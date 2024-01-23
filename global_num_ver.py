819a3a7cbcd3ee7f0419f59077e30fe3265cbb87
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-01-23 12:11:01 +0530

    Add files via upload

---
+++ global_num_ver.py
@@ -0,0 +1,20 @@
+# global variable and global keyword:
+num = 50
+print(num)
+
+num = 80 # excecute 1 num
+def script():
+    num=20
+    print(num)
+script()
+
+num = 100 # execute 2 num
+def hi():
+    num=300
+    print(num)
+print(num)
+hi()
+
+
+
+
